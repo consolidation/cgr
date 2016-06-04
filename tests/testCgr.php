8eb5c45ed193fd08ebf71b98eee9afb644a3b7c8
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-06-03 19:14:40 -0700

    Add a test of a pass-through command calling 'composer init'.

--- tests/testCgr.php
+++ tests/testCgr.php
@@ -29,7 +29,7 @@ class CgrTests extends \PHPUnit_Framework_TestCase
         static::fileDeleteRecursive(static::$tempDir);
     }
 
-    public function testApplicationValues()
+    public function testApplicationCommandStringsValues()
     {
         $argvCgrMultipleProjectForms = array(
             'cgr',
@@ -72,9 +72,9 @@ EOT;
      * arguments, and check to see if the generate appropriate
      * command strings.
      *
-     * @dataProvider testApplicationValues
+     * @dataProvider testApplicationCommandStringsValues
      */
-    public function testApplication($argv, $expected)
+    public function testApplicationCommandStrings($argv, $expected)
     {
         $commandList = $this->application->parseArgvAndGetCommandList($argv, '/home/user');
         $commandStrings = array();
@@ -85,43 +85,9 @@ EOT;
         $this->assertEquals($expected, $actual);
     }
 
-
-    /**
-     * Functional test using a mocked application: we call a
-     * script that echos its arguments and prints the COMPOSER_BIN_DIR
-     * environment variable.
-     */
-    public function testEnvironmentVariableSetting()
+    public function testApplicationOutputValues()
     {
-        $argv = array(
-            'cgr',
-            '--composer-path',
-            'php ' . __DIR__ . '/echoPrintenv.php',
-            '--bin-dir',
-            $this->workDir . '/.composer/bin',
-            '--cgr-output',
-            $this->workDir . '/output.txt',
-            'a/b:1.0'
-        );
-
-        $exitCode = $this->application->run($argv, $this->workDir);
-        $this->assertFileExists($this->workDir . '/output.txt', 'Output file created.');
-        $output = file_get_contents($this->workDir . '/output.txt');
-        $expected = <<< EOT
---working-dir={workdir}/.composer/global/a/b require a/b:1.0
-{workdir}/.composer/bin
-EOT;
-        $expected = str_replace('{workdir}', $this->workDir, $expected);
-        $this->assertEquals($expected, rtrim($output));
-    }
-
-    /**
-     * Functional test using a mocked application: we call 'echo' in
-     * place of composer and see if the output looks correct.
-     */
-    public function testApplicationFunctional()
-    {
-        $argv = array(
+        $argvEcho = array(
             'cgr',
             '--composer-path',
             'echo',
@@ -130,15 +96,51 @@ EOT;
             'x/y:1.0',
             'a/b=~2'
         );
-
-        $exitCode = $this->application->run($argv, $this->workDir);
-        $this->assertEquals(0, $exitCode);
-        $this->assertFileExists($this->workDir . '/output.txt', 'Output file created.');
-        $output = file_get_contents($this->workDir . '/output.txt');
-        $expected = <<< EOT
+        $expectedEcho = <<< EOT
 --working-dir={workdir}/.composer/global/x/y require x/y:1.0
 --working-dir={workdir}/.composer/global/a/b require a/b:~2
 EOT;
+
+        $argvEchoPrintenv = array(
+            'cgr',
+            '--composer-path',
+            'php ' . __DIR__ . '/echoPrintenv.php',
+            '--bin-dir',
+            '/a/b/.composer/bin',
+            'a/b:1.0'
+        );
+        $expectedEchoPrintenv = <<< EOT
+--working-dir={workdir}/.composer/global/a/b require a/b:1.0
+/a/b/.composer/bin
+EOT;
+
+        return array(
+            array(
+                $argvEcho,
+                $expectedEcho,
+            ),
+            array(
+                $argvEchoPrintenv,
+                $expectedEchoPrintenv,
+            ),
+        );
+    }
+
+    /**
+     * Functional test using a mocked application: we call a
+     * script that echos its arguments and prints the COMPOSER_BIN_DIR
+     * environment variable.
+     *
+     * @dataProvider testApplicationOutputValues
+     */
+    public function testApplicationOutput($argv, $expected)
+    {
+        $argv[] = '--cgr-output';
+        $argv[] = $this->workDir . '/output.txt';
+
+        $exitCode = $this->application->run($argv, $this->workDir);
+        $this->assertFileExists($this->workDir . '/output.txt', 'Output file created.');
+        $output = file_get_contents($this->workDir . '/output.txt');
         $expected = str_replace('{workdir}', $this->workDir, $expected);
         $this->assertEquals($expected, rtrim($output));
     }
@@ -154,7 +156,7 @@ EOT;
     public function testApplicationWithComposer()
     {
         $argv = array(
-            'cgr',
+            'composer',
             '--no-update',
             'consolidation/cgr:1.0',
         );
@@ -163,6 +165,34 @@ EOT;
         $this->assertFileExists($this->workDir . '/.composer/global/consolidation/cgr/composer.json', 'composer.json created');
         $composerJson = file_get_contents($this->workDir . '/.composer/global/consolidation/cgr/composer.json');
         $this->assertContains('"consolidation/cgr": "1.0"', $composerJson);
+    }
+    /**
+     * Functional test with the real composer executable.  Use cgr to
+     * require cgr, although we do so with --no-update to avoid pointlessly
+     * downloading files we do not actually need.  We therefore only
+     * test to see whether the `composer.json` file is correctly updated;
+     * we cannot test to see if the cgr binary is correctly installed
+     * in the appropriate bin directory, as this step is not done.
+     */
+    public function testApplicationWithComposerPassthruCommand()
+    {
+        $argv = array(
+            'composer',
+            'init',
+            "--name=test/test",
+            '--no-interaction',
+        );
+        $exitCode = $this->application->run($argv, $this->workDir);
+        $this->assertEquals(0, $exitCode);
+        $this->assertFileExists($this->workDir . '/composer.json', 'composer.json created');
+        $composerJson = file_get_contents($this->workDir . '/composer.json');
+        $expected = <<<EOT
+{
+    "name": "test/test",
+    "require": {}
+}
+EOT;
+        $this->assertEquals($expected, rtrim($composerJson));
     }
 
     static function tempdir($baseDir = false, $prefix = '')
